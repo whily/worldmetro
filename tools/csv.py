0656260fdd7aba0707dc659bc66da9d39c9908da
Yujian Zhang <yujian.zhang@gmail.com>
2014-07-12 01:27:23 +0800

    Update gtfs.py and csv.py for GTFS schemas.

--- tools/csv.py
+++ tools/csv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Handle CSV file.
+# Handle CSV file according to http://tools.ietf.org/html/rfc4180
 
 class CSV:
     def __init__(self, file):
-        self.lines = file.readlines()
+        lines = [line.decode("utf-8").split(",") for line in file.readlines()]
+        self.header = lines[0]
+        self.records =  lines[1:]
--- tools/gtfs.py
+++ tools/gtfs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Download GTFS data, convert to sqlite3 db per city.
+# Reference: https://developers.google.com/transit/gtfs/reference
 
 import sqlite3 as sqlite
 import sys
@@ -8,19 +9,53 @@ import zipfile
 import csv
 
 def get_city(city_name, gtfs_name):
+    print(city_name)
     db = city_name + ".db"
     con = sqlite.connect(db)
     z = zipfile.ZipFile(gtfs_name, "r", zipfile.ZIP_DEFLATED)
 
     with con:
         cur = con.cursor()    
-        cur.execute("CREATE TABLE stops(stop_id TEXT, stop_name TEXT, stop_lat TEXT, stop_lon TEXT)")
+
+        # Schemas according to mandatory fields of mandatory files from GTFS.
+        cur.execute('''CREATE TABLE agency(agency_name TEXT, 
+                                           agency_url TEXT NOT NULL, 
+                                           agency_timezone TEXT NOT NULL)''')       
+        cur.execute('''CREATE TABLE stops(stop_id TEXT PRIMARY KEY, 
+                                          stop_name TEXT NOT NULL, 
+                                          stop_lat TEXT NOT NULL, 
+                                          stop_lon TEXT NOT NULL)''')
+        cur.execute('''CREATE TABLE routes(route_id TEXT PRIMARY KEY, 
+                                           route_short_name TEXT NOT NULL, 
+                                           route_long_name TEXT NOT NULL, 
+                                           route_type INTEGER NOT NULL)''')
+        # Move table calender before trips since file calendar.txt should be processed first for service_id.
+        cur.execute('''CREATE TABLE calendar(service_id TEXT PRIMARY KEY, 
+                                             monday INTEGER NOT NULL, 
+                                             tuesday INTEGER NOT NULL, 
+                                             wednesday INTEGER NOT NULL, 
+                                             thursday INTEGER NOT NULL, 
+                                             friday INTEGER NOT NULL, 
+                                             saturday INTEGER NOT NULL, 
+                                             sunday INTEGER NOT NULL, 
+                                             start_date TEXT NOT NULL, 
+                                             end_date TEXT NOT NULL)''')                                 
+        cur.execute('''CREATE TABLE trips(route_id TEXT REFERENCES routes(route_id), 
+                                          service_id TEXT REFERENCES calendar(service_id), 
+                                          trip_id TEXT PRIMARY KEY)''')
+        cur.execute('''CREATE TABLE stop_times(trip_id TEXT REFERENCES trips(trip_id), 
+                                               arrival_time TEXT NOT NULL, 
+                                               departure_time TEXT NOT NULL, 
+                                               stop_id TEXT REFERENCES stops(stop_id), 
+                                               stop_sequence TEXT NOT NULL)''')
+
         stops = csv.CSV(z.open("stops.txt"))
-        lines = stops.lines
-        for line in lines[1:]:     # Skip the caption
-            fields = line.decode("utf-8").split(",")
-            print(fields[0])
-            cur.execute("INSERT INTO stops VALUES('101', 'Van Cortlandt Part - 242 St', '40.889248', '-73.898583')")
+        records = stops.records
+        for record in records: 
+            fields = record
+            cur.execute("INSERT INTO stops VALUES(:stop_id, :stop_name, :stop_lat, :stop_lon)", 
+                        {'stop_id': fields[0], 'stop_name': fields[2], 
+                         'stop_lat': fields[4], 'stop_lon': fields[5]})
 
 if __name__ == '__main__': 
     city = 'new_york'
